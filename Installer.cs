1caa478ae23e05517eeffccd279dba1c2e4c37bd
Baran Karakaya <thebarankarakaya@gmail.com>
2024-07-03 20:12:42 +0300

    Update installation

--- Installer.cs
+++ Installer.cs
@@ -1,55 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace directx_installer
 {
-    public static class Installer { 
+    public static class Installer {
 
-        static string app_title = "\n     DirectX Runtime Installer for Windows\n     https://github.com/barankrky/directx_installer \n";
+        static NotifyIcon notifyIcon = new NotifyIcon();
 
-        static void CheckPackages()
+        static void NotifyIconStarted()
         {
-            Console.WriteLine("- Checking directx...");
-            Thread.Sleep(250);
-            if (Directory.Exists("directx"))
-            {
-                DirectoryInfo package_dir = new DirectoryInfo("directx");
-                FileInfo[] setups = package_dir.GetFiles("*setup*.exe");
-                FileInfo[] cab_files = package_dir.GetFiles("*.cab");
-                FileInfo[] dll_files = package_dir.GetFiles("*.dll");
+            notifyIcon.Icon = IconExtractor.Extract("imageres.dll", 152, true);
+            notifyIcon.Text = "Installation started.";
+            notifyIcon.BalloonTipTitle = Properties.Resources.console_title;
+            notifyIcon.BalloonTipText = "Installation started. Please wait until it is completed.";
+            notifyIcon.Visible = true;
+            notifyIcon.ShowBalloonTip(3000);
 
-                List<FileInfo> directx_files = new List<FileInfo>();
-                directx_files.AddRange(setups);
-                directx_files.AddRange(cab_files);
-                directx_files.AddRange(dll_files);
+        }
 
-                foreach (FileInfo file in directx_files)
-                {
-                    Console.WriteLine("-- " + file.Name);
-                }
-            }
-            else
-            {
-                Console.WriteLine("-- 'directx' not found. Please ensure that there is a 'directx' folder in the location where you run the application.");
-                Console.ReadLine();
-                Environment.Exit(0);
-            }
-
-            Console.Clear();
-            Console.WriteLine(app_title);
+        static void NotifyIconFinished()
+        {
+            notifyIcon.Icon = IconExtractor.Extract("imageres.dll", 232, true);
+            notifyIcon.Text = "Installation finished.";
+            notifyIcon.BalloonTipTitle = Properties.Resources.console_title;
+            notifyIcon.BalloonTipText = "Installation completed successfully.";
+            notifyIcon.ShowBalloonTip(3000);
 
         }
 
         static void Install()
         {
             Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory + "directx";
-            Console.Write("--> Installing DirectX...");
             ProcessStartInfo psi = new ProcessStartInfo();
             psi.FileName = "cmd.exe";
             psi.Arguments = @"/c .Setup.exe /silent";
@@ -64,21 +47,13 @@ namespace directx_installer
             proc.Start();
             proc.WaitForExit();
 
-            Console.Clear();
-            Console.WriteLine(app_title);
-            Console.WriteLine(">>> DirectX Runtime installed successfully.");
-            Console.WriteLine(">>> Closing in 3 seconds.");
-            Thread.Sleep(2850);
-            Environment.Exit(0);
-
         }
 
         public static void Start()
         {
-            Console.Title = "DirectX Runtime Installer by barankrky";
-            Console.WriteLine(app_title);
-            CheckPackages();
+            NotifyIconStarted();
             Install();
+            NotifyIconFinished();
         }
     }
 }
